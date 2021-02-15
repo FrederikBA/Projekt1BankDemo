5a596902c40d60fe7f00ef50f0463bfb53fa3a66
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-02-15 20:27:27 +0100

    Updated sql resource

--- src/main/resources/BankQuery.sql
+++ src/main/resources/BankQuery.sql
@@ -1,8 +1,21 @@
 create database ebberoedbank;
 
+drop table customers;
 create table customers(
 customerID int not null primary key auto_increment,
 customerName varchar(255) not null
+);
+
+drop table accounts;
+create table accounts(
+accountID int not null primary key auto_increment,
+balance int not null
+);
+
+drop table transactions;
+create table transactions(
+transactionID int not null primary key auto_increment,
+amount int not null
 );
 
 
