77b80ff6022bc1471b2da3642ed65eb393a003bd
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-02-15 15:05:37 +0100

    added scanner functionallity

--- src/main/java/Controller/MainController.java
+++ src/main/java/Controller/MainController.java
@@ -10,6 +10,8 @@ public class MainController {
     Scanner sc = new Scanner(System.in);
     IUI ui = new UI();
     int choice = 0;
+    Customer c1 = new Customer("John");
+    Account a1 = new Account(c1);
 
     public void runProgram() {
         while (choice != 9) {
@@ -17,7 +19,23 @@ public class MainController {
             choice = sc.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.println("Stuff");
+                    System.out.print("Enter deposit amount: ");
+                    try {
+                        a1.depositAmount(sc.nextInt());
+                    } catch (BankException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 2:
+                    System.out.print("Enter withdrawal amount: ");
+                    try {
+                        a1.withdrawAmount(sc.nextInt());
+                    } catch (BankException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 3:
+                    System.out.println(a1.getBalance());
                     break;
                 default:
                     ui.showExitMessage();
--- src/main/java/UI/UI.java
+++ src/main/java/UI/UI.java
@@ -3,7 +3,9 @@ package UI;
 public class UI implements IUI {
 
     public void showUserMenu() {
-        System.out.println("1) Do stuff");
+        System.out.println("1) Deposit amount");
+        System.out.println("2) Withdraw amount");
+        System.out.println("3) Show account funds");
         System.out.println("9) Exit");
     }
 
