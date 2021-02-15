af4d4de8b7112e6e98e570f8d52247f27a359866
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-02-15 20:10:45 +0100

    fix to sql resource

--- src/main/resources/BankQuery.sql
+++ src/main/resources/BankQuery.sql
@@ -1,1 +1,14 @@
 create database ebberoedbank;
+
+create table customers(
+customerID int not null primary key auto_increment,
+customerName varchar(255) not null
+);
+
+
+/*INSERT STATEMENTS */
+INSERT INTO customers(customerID,customerName) VALUES (?,?);
+
+/*SELECT STATEMENTS */
+SELECT * FROM customers;
+
